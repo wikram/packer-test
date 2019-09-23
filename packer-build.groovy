7f0b5b135480c1487aa517123814da47ffbf2742
wikram <wikram.patankar@gmail.com>
2019-09-23 20:02:30 +0530

    Update packer-build.groovy

--- packer-build.groovy
+++ packer-build.groovy
@@ -58,7 +58,7 @@ node(WhichNode)
         sh "wget https://raw.githubusercontent.com/wikram/packer-test/master/packer.json"
         withCredentials([azureServicePrincipal('sandbox-packer')]) {
             sh """
-            /sbin/packer build -force -var \"client_secret=${AZURE_CLIENT_ID}\" -var-file=creds.json packer.json  2>&1 | tee packer_output.log"""
+            /sbin/packer build -force -var-file=creds.json packer.json  2>&1 | tee packer_output.log"""
         } 
         sh "pwd"
         sh "ls -l"
