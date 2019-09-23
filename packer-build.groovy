7531500a1b39aaede1ad468d5f49411cde492512
wikram <wikram.patankar@gmail.com>
2019-09-23 23:35:12 +0530

    Update packer-build.groovy

--- packer-build.groovy
+++ packer-build.groovy
@@ -79,7 +79,8 @@ node(WhichNode)
                                         tenantIdVariable: 'TENANT_ID')]) {
                 sh 'az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET -t $TENANT_ID'
                 sh 'az account set -s $SUBS_ID'
-                sh 'az resource list'
+                //sh 'az resource list'
+                sh 'az vm create -g ${Resource_group_name} -n Test-Vm --image ${ImageName}'
                 }
             }
             else
@@ -91,7 +92,8 @@ node(WhichNode)
                                         tenantIdVariable: 'TENANT_ID')]) {
                 sh 'az login --service-principal -u $CLIENT_ID -p $CLIENT_SECRET -t $TENANT_ID'
                 sh 'az account set -s $SUBS_ID'
-                sh 'az resource list'
+                //sh 'az resource list'
+                sh 'az vm create -g ${Resource_group_name} -n Test-Vm --image ${ImageName}'
                 }
             }
     }
